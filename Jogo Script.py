1845a2809136c16e26e359417d00fea6aa8155ef
xReb0rn <anderson.nobrega62@gmail.com>
2017-04-17 20:19:24 -0300

    Novas Funções

--- Jogo Script.py
+++ Jogo Script.py
@@ -12,24 +12,74 @@ preto = (0, 0, 0)
 branco = (255, 255, 255)
 cinza = (160, 160, 160)
 
+texto_grande = pygame.font.Font('assets/arial.ttf', 115)
+texto_médio = pygame.font.Font('assets/arial.ttf', 40)
+texto_pequeno = pygame.font.Font('assets/arial.ttf', 20)
+
 resolução_jogo = pygame.display.set_mode((resolução_altura, resolução_altura))
 pygame.display.set_caption('Where in the world is carmen sandiego')
-relogio = pygame.time.Clock()
+fps = pygame.time.Clock()
 
 def sair_jogo():
+    #Sair do modulo pygame e do jogo
     pygame.quit()
     quit()
 
-def button(msg, x, y, w, h, ic, ac, ação=None):
+def objetos_texto(texto, fonte):
+    textSurface = fonte.render(texto, True, branco)
+    return textSurface, textSurface.get_rect()
+
+def botões(msg, x, y, largura, altura, cor_escura, cor_clara, ação=None):
+    #Botões que vão ser usados no jogo
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-    if x + w > mouse[0] > x and y + h > mouse[1] > y:
-        pygame.draw.rect(resolução_jogo, ac, (x, y, w, h))
+    if x + largura > mouse[0] > x and y + altura > mouse[1] > y:
+        pygame.draw.rect(resolução_jogo, cor_clara, (x, y, largura, altura))
         if click[0] == 1 and ação != None:
             ação()
     else:
-        pygame.draw.rect(resolução_jogo, ic, (x, y, w, h))
-    smallText = pygame.font.SysFont("comicsansms", 20)
-    textSurf, textRect = text_objects(msg, smallText)
-    textRect.center = ((x + (w / 2)), (y + (h / 2)))
-    resolução_jogo.blit(textSurf, textRect)
+        pygame.draw.rect(resolução_jogo, cor_escura, (x, y, largura, altura))
+
+    superficie_texto, retangulo_texto = objetos_texto(msg, texto_pequeno)
+    retangulo_texto.center = ((x + (largura / 2)), (y + (altura / 2)))
+    resolução_jogo.blit(superficie_texto, retangulo_texto)
+
+def menu_jogo():
+    #Menu principal do jogo
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sair_jogo()
+
+        resolução_jogo.fill(preto)
+        #superficie_texto, retangulo_texto = objetos_texto('Where in the world is carmen sandiego', texto_médio)
+        #retangulo_texto.center = ((resolução_largura / 2), (resolução_altura / 2))
+        #resolução_jogo.blit(superficie_texto, retangulo_texto)
+
+        botões('Novo Jogo', 250, 200, 150, 50, preto, cinza, loop_jogo)
+        #botões('Carregar Jogo', 300, 250, 150, 50, preto, cinza, carregar_jogo)
+        botões('Sair', 250, 300, 150, 50, preto, cinza, sair_jogo)
+
+        pygame.display.update()
+        fps.tick(15)
+
+def loop_jogo():
+    #Loop do jogo
+
+    saiu_jogo = False
+
+    while not saiu_jogo:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                sair_jogo()
+
+
+        pygame.display.update()
+        fps.tick(60)
+
+menu_jogo()
+loop_jogo()
+sair_jogo()
